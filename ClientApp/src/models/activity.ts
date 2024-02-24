514bf75386b64ec18fe75ef16776c4d4f2f5c3b8
Cyberpatinho <edsonkohisa@gmail.com>
2024-02-24 20:16:14 -0300

    Added rudimentary CRUD for activities in React (not yet integrated with back-end)

---
+++ ClientApp/src/models/activity.ts
@@ -0,0 +1,10 @@
+export interface Activity {
+    id: string
+    title: string
+    description: string
+    date: string
+    category: string
+    country: string
+    city: string
+    venue: string
+  }
