a17071e9c40ce32de75ed94da5e4816a83072acd
Cyberpatinho <edsonkohisa@gmail.com>
2024-03-24 15:14:57 -0300

    Added routing to client project

--- ClientApp/src/features/activities/dashboard/ActivityDashboard.tsx
+++ ClientApp/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,14 +1,21 @@
 import { Grid, GridColumn, } from "semantic-ui-react";
 import ActivityList from "./ActivityList";
-import ActivityDetails from "../details/ActivityDetails";
-import ActivityForm from "../form/ActivityForm";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
+import { useEffect } from "react";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 
 export default observer(function ActivityDashboard() {
 
     const {activityStore} = useStore();
-    const {selectedActivity, editMode} = activityStore;
+    const { loadActivities, activityRegistry } = activityStore;
+
+    useEffect(() => {
+        if (activityRegistry.size === 0) loadActivities();
+    }, [loadActivities, activityRegistry.size]);
+  
+    if (activityStore.loadingInitial)
+      return <LoadingComponent content='Loading app' />
 
     return (
         <Grid>
@@ -16,10 +23,7 @@ export default observer(function ActivityDashboard() {
                 <ActivityList />
             </GridColumn>
             <GridColumn width='6'>
-                {selectedActivity && !editMode &&
-                <ActivityDetails />}
-                {editMode &&
-                <ActivityForm />}
+                <h2>Activity filters</h2>
             </GridColumn>
         </Grid>
     )
